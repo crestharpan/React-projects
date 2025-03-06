80249dedd01a421e2c9075d1f300922ce9b3c664
crestharpan <arpanshrestha225@gmail.com>
2025-03-06 14:45:00 +0545

    stats-functionality-added

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -26,7 +26,7 @@ export default function App() {
         onDelete={handleDeleteItem}
         onToggle={handleToggleItem}
       />{" "}
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -106,11 +106,16 @@ function Item({ item, onDelete, onToggle }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const packedItems = items.filter((item) => item.packed === true);
+  const numPackedItems = packedItems.length;
+  const numItems = items.length;
+
   return (
     <footer className="stats">
       <em>
-        You have x items on your list and you have already packed x items (x%)
+        You have {numItems} items on your list and you have already packed{" "}
+        {numPackedItems} items ({(numPackedItems / numItems) * 100}%)
       </em>
     </footer>
   );
