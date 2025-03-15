187b8dc61aaa5503855a84f5f192c24ead8f579e
crestharpan <arpanshrestha225@gmail.com>
2025-03-15 13:51:43 +0545

    inlcuding the friend list

--- eat-and-split/src/App.js
+++ eat-and-split/src/App.js
@@ -18,3 +18,41 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />{" "}
+      </div>
+    </div>
+  );
+}
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">{`You owe your friend $${Math.abs(
+          friend.balance
+        )}`}</p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">{`your friend owe you $${friend.balance}`}</p>
+      )}
+      {friend.balance === 0 && <p>{`you and ${friend.name} are even`}</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
