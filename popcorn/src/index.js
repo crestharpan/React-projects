084024dccf6aa884d3ec9c30d01fe2b8c5604344
crestharpan <arpanshrestha225@gmail.com>
2025-07-25 22:17:54 +0545

    fetching omdb API

--- popcorn/src/index.js
+++ popcorn/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./starRating";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./starRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating
-      maxSize={5}
-      message={["Terrible", "Bad", "okay", "Good", "Amazing"]}
-    />
-    <StarRating maxSize={5} color={"red"} />
-    <StarRating maxSize={5} color={"blue"} defaultRating={2} />
+    <App />
   </React.StrictMode>
 );
